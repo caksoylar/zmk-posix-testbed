d8d834c69bed380b9b7c46972e819d33b4d0370e
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-04-14 14:40:11 -0700

    Update screen from upstream

--- boards/shields/zen_screen/custom_status_screen.c
+++ boards/shields/zen_screen/custom_status_screen.c
@@ -55,23 +55,23 @@ lv_obj_t *zmk_display_status_screen() {
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
-                               lv_theme_get_font_small(screen), LV_PART_MAIN);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
-
     lv_obj_t *LayersHeading;
     LayersHeading = lv_img_create(screen);
-    lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 8, 5);
+    lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 0, -30);
     lv_img_set_src(LayersHeading, &layers2);
+
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
+                               &lv_font_montserrat_16, LV_PART_MAIN);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
 #endif
 
-// #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-//     lv_obj_t *zenlogo_icon;
-//     zenlogo_icon = lv_img_create(screen);
-//     lv_img_set_src(zenlogo_icon, &zenlogo);
-//     lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 2, -5);
-// #endif
+/*#if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    lv_obj_t *zenlogo_icon;
+    zenlogo_icon = lv_img_create(screen);
+    lv_img_set_src(zenlogo_icon, &zenlogo);
+    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 2, -5);
+#endif*/
 
     return screen;
 }
